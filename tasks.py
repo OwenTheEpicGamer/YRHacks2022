0f28db2b5e3f6cd73ce54122709eaf6ed359830c
Austin Zeng <austinzeng@gmail.com>
2022-04-09 11:11:58 -0400

    Implement deadlines

--- tasks.py
+++ tasks.py
@@ -3,47 +3,67 @@ from discord.ext import commands
 from discord import Color, Embed
 from pymongo import ReturnDocument
 from random import randint
+from datetime import datetime
+import dateparser
+import timeago
 
-class Points(commands.Cog):
+class Tasks(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.db = get_database()
 
     @commands.command(name='addtask')
-    async def addtask(self, ctx: commands.Context, name: str, *description):
-        if not description:
-            description = ('no description',)
+    async def addtask(self, ctx: commands.Context, name: str, deadline: str):
+        if len(name) > 100:
+            await ctx.send(f'{ctx.author.mention} task name is too long!')
+            return
+        deadline = dateparser.parse(deadline, settings = {
+            'TO_TIMEZONE': 'UTC',
+            'PREFER_DATES_FROM': 'future'
+        })
+        if deadline is None:
+            await ctx.send(f'{ctx.author.mention} invalid deadline!')
+            return
         res = self.db.tasks.update_one(
             { 'user_id': ctx.author.id, 'name': name },
-            { '$set': { 'description': ' '.join(description) } },
+            { '$set': { 'deadline': deadline } },
             upsert=True
         )
-        await ctx.send(f'{ctx.author.mention} task {name} {"added" if res.upserted_id else "updated"} successfully!')
+        await ctx.send(f'{ctx.author.mention} task **{name}** {"added" if res.upserted_id else "updated"} successfully!')
 
     @commands.command(name='done')
     async def done(self, ctx: commands.Context, name: str):
-        res = self.db.tasks.delete_one({ 'user_id': ctx.author.id, 'name': name })
-        if res.deleted_count:
-            pts = randint(3, 7)
-            user = self.db.users.find_one_and_update(
-                { 'user_id': ctx.author.id },
-                { '$inc' : { 'points': pts } },
-                upsert=True,
-                return_document=ReturnDocument.AFTER
-            )
-            await ctx.send(f'{ctx.author.mention} task {name} completed!\n'
-                + f'You earned {pts} points (current points: {user["points"]})')
+        task = self.db.tasks.find_one_and_delete({ 'user_id': ctx.author.id, 'name': name })
+        if task is None:
+            await ctx.send(f'{ctx.author.mention} task **{name}** does not exist!')
         else:
-            await ctx.send(f'{ctx.author.mention} task {name} does not exist!')
+            now = datetime.utcnow()
+            if task['deadline'] >= now:
+                pts = randint(3, 7)
+                user = self.db.users.find_one_and_update(
+                    { 'user_id': ctx.author.id },
+                    { '$inc' : { 'points': pts } },
+                    upsert=True,
+                    return_document=ReturnDocument.AFTER
+                )
+                feedback = f'You earned {pts} points (current points: {user["points"]})'
+            else:
+                feedback = "It was overdue, so you didn't earn any points"
+            await ctx.send(f'{ctx.author.mention} task **{name}** completed!\n' + feedback)
 
     @commands.command(name='tasks')
     async def tasks(self, ctx: commands.Context):
         embed = Embed(title='Your Tasks', color=Color.green())
-        for x in self.db.tasks.find({ 'user_id': ctx.author.id }):
-            embed.add_field(name=x['name'], value=x['description'], inline=False)
-        await ctx.send(embed=embed)
+        user_tasks = self.db.tasks.find({ 'user_id': ctx.author.id }).sort('deadline')
+        now = datetime.utcnow()
+        for x in user_tasks:
+            embed.add_field(name=x['name'], value='due ' + timeago.format(x['deadline'], now), inline=False)
+        if user_tasks.retrieved == 0:
+            await ctx.send(f'{ctx.author.mention} tasks all done!')
+        else:
+            await ctx.send(embed=embed)
 
 
 def setup(bot: commands.Bot):
-    bot.add_cog(Points(bot))
+    bot.add_cog(Tasks(bot))
